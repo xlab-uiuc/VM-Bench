ebfc3d7e7471e8cc7e83e77ca2856d8fab38db7f
Siyuan <siyuanc3@illinois.edu>
2023-06-11 17:02:59 -0500

    sysbench build

---
+++ build_bench.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./build_scripts/build_sysbench.sh
---
+++ build_scripts/build_graphBIG.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# instal dependency package if needed
+# apt -y install make automake libtool pkg-config libaio-dev
+
+BASEDIR=`pwd`
+cd $BASEDIR/sysbench
+
+echo "Building graphBIG..."
+./autogen.sh
+./configure --without-mysql
+make -j `nproc`
+
+if test -f "$BASEDIR/sysbench/src/sysbench"; then
+    echo "Done building sysbench!"
+else
+    echo "Fail to build sysbench"
+fi
+
