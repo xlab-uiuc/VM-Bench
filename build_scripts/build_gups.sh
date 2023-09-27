9d2f660ed9d1b35bcce8ee37033d319969f759cd
Siyuan <siyuanc3@illinois.edu>
2023-09-27 14:28:10 -0500

    fix gups runtime error for logtable leve > 30

--- build_scripts/build_gups.sh
+++ build_scripts/build_gups.sh
@@ -6,19 +6,16 @@
 BASEDIR=`pwd`
 cd $BASEDIR/gups
 
-if which mpicc; then
-    echo "mpicc ready."
-else
-    # if you don't have openmpi library installed
-    sudo apt install libhdf5-mpich-dev
-fi
+# install mpich if needed
+sudo apt install -y libmpich-dev
 
 # apply patch to add include path of mpich
-if ! grep "CCFLAGS = *mpich" Makefile.linux; then
-    cp $BASEDIR/patches/gups_makefile.patch .
-    git apply gups_makefile.patch
-fi
+# if ! grep "^CCFLAGS.*mpich" Makefile.linux; then
+#     cp $BASEDIR/patches/gups_makefile.patch .
+#     git apply gups_makefile.patch
+# fi
 
+cp $BASEDIR/patches/gups_vanilla_patch_for_large_mem.c gups_vanilla.c
 make -f Makefile.linux gups_vanilla;
 
 if test -f "$BASEDIR/gups/gups_vanilla"; then
