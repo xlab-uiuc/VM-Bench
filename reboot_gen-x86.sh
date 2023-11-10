846213bf5c120b4ef1a11de671154f6550a3393b
Siyuan <siyuanc3@illinois.edu>
2023-11-10 14:14:28 -0600

    instruction to get reboot menuentry

--- reboot_gen-x86.sh
+++ reboot_gen-x86.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# get menuentry from 
+# awk -F\' '/menuentry / {print $2}' /boot/grub/grub.cfg
+# source https://adamtheautomator.com/ubuntu-grub
+
 sudo grub-reboot "Advanced options for Ubuntu>Ubuntu, with Linux 5.15.0-gen-x86"
 sudo reboot
