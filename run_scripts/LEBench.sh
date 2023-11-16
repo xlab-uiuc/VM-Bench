fe48d56fc7ed14f4b37c4aef964637d6d0531153
Siyuan Chai <siyuanc3@illinois.edu>
2023-11-16 02:08:01 -0500

    LeBench performance run script

---
+++ run_scripts/LEBench.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# See https://github.com/xlab-uiuc/LEBench/blob/master/README.md for parameter setup
+
+cd LEBench;
+COMMAND_CPU=8
+sudo LEBENCH_DIR=. taskset -ac $COMMAND_CPU python2 run.py
+
+KERNEL_NAME=`uname -r`
+THP_CONFIG="no_thp_support"
+thp_file="/sys/kernel/mm/transparent_hugepage/enabled"
+
+# Check if the file exists
+if [[ -f $thp_file ]]; then
+    # Read the content of the file
+    content=$(cat $thp_file)
+    
+    # Check if 'always' or 'madvise' is active (indicated by the [brackets])
+    if [[ $content == *"[always]"* ]]; then
+        THP_CONFIG="THP_always"
+    elif [[ $content == *"[madvise]"* ]]; then
+        THP_CONFIG="THP_madvise"
+    elif [[ $content == *"[never]"* ]]; then
+        THP_CONFIG="THP_never"
+    else
+        echo "Unable to determine the status of Transparent Huge Pages."
+    fi
+else
+    echo "Transparent Huge Pages is not supported on this system."
+fi
+
+TIME=`date "+%Y-%m-%d-%H:%M:%S"`
+FILE_PREFIX=${KERNEL_NAME}_${THP_CONFIG}_LEBench_${TIME}
+
+mv .new_output_file.csv ../paper_results/${KERNEL_NAME}/${FILE_PREFIX}.csv
