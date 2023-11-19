Fix bench.py and perf.py to record performance and page walk latency nuumber for each trial instead of only outputing the average.
Now perf.py handles LEBench and app benchmarks at the same time.
sudo python run_scripts/perf.py --bench_suite LEBench
sudo python run_scripts/perf.py --bench_suite app

run_scripts/bench.py
@@ -5,29 +5,36 @@ import subprocess
 import datetime
 from typing import NamedTuple
 
+import numpy as np
+import pandas as pd
+
+RUNTIMES=5
 benchmarks = {
     # bench_name, relative bench script path, times, regex of the loadtime, regex of the runtime
-    "Graph - BC"  : ("./graphbig_bc.sh"       , 5, r"time: (.*) sec", r"time: (.*) sec\n="),
-    "Graph - BFS" : ("./graphbig_bfs.sh"      , 5, r"time: (.*) sec", r"time: (.*) sec\n="),
-    "Graph - DFS" : ("./graphbig_dfs.sh"      , 5, r"time: (.*) sec", r"time: (.*) sec\n="),
-    "Graph - DC"  : ("./graphbig_dc.sh"       , 5, r"time: (.*) sec", r"time: (.*) sec\n="),
-    "Graph - SSSP": ("./graphbig_sssp.sh"     , 5, r"time: (.*) sec", r"time: (.*) sec\n="),
-    "Graph - CC"  : ("./graphbig_cc.sh"       , 5, r"time: (.*) sec", r"time: (.*) sec\n="),
-    "Graph - TC"  : ("./graphbig_tc.sh"       , 5, r"time: (.*) sec", r"time: (.*) sec\n="),
-    "Graph - PR"  : ("./graphbig_pagerank.sh" , 5, r"time: (.*) sec", r"time: (.*) sec\n="),
-    "sysbench"    : ("./sysbench.sh"          , 5, None             , r"time elapsed:\s+(.*)s"),
-    "gups"        : ("./gups.sh"              , 5, None             , r"Update time \(secs\):    (.*)"),
-    # "gups"        : ("./gups.sh"              , 5, None             , r"Gups:  (.*)"),
-    "mummer"      : ("./mummer.sh"            , 5, None             , r"real (.*) user"),
+    "Graph - BC"  : ("./graphbig_bc.sh"       , RUNTIMES, r"time: (.*) sec", r"time: (.*) sec\n="),
+    "Graph - BFS": ("./graphbig_bfs.sh", RUNTIMES, r"time: (.*) sec", r"time: (.*) sec\n="),
+    "Graph - DFS": ("./graphbig_dfs.sh", RUNTIMES, r"time: (.*) sec", r"time: (.*) sec\n="),
+    "Graph - DC"  : ("./graphbig_dc.sh"       , RUNTIMES, r"time: (.*) sec", r"time: (.*) sec\n="),
+    "Graph - SSSP": ("./graphbig_sssp.sh"     , RUNTIMES, r"time: (.*) sec", r"time: (.*) sec\n="),
+    "Graph - CC"  : ("./graphbig_cc.sh"       , RUNTIMES, r"time: (.*) sec", r"time: (.*) sec\n="),
+    "Graph - TC"  : ("./graphbig_tc.sh"       , RUNTIMES, r"time: (.*) sec", r"time: (.*) sec\n="),
+    "Graph - PR"  : ("./graphbig_pagerank.sh" , RUNTIMES, r"time: (.*) sec", r"time: (.*) sec\n="),
+    "sysbench"    : ("./sysbench.sh"          , RUNTIMES, None             , r"time elapsed:\s+(.*)s"),
+    "gups"        : ("./gups.sh"              , RUNTIMES, None             , r"Update time \(secs\):    (.*)"),
+    # # "gups"        : ("./gups.sh"              , RUNTIMES, None             , r"Gups:  (.*)"),
+    "mummer"      : ("./mummer.sh"            , RUNTIMES, None             , r"real (.*) user"),
 }
 
 
-FILENAME = ''
-SCRIPT_DIR = ''
+FILENAME = ""
+SCRIPT_DIR = ""
+
 
 def run_command(cmd):
     command_cpu = "8"
-    r = subprocess.run(["sudo", "taskset", "-ac", command_cpu, cmd], capture_output=True)
+    r = subprocess.run(
+        ["sudo", "taskset", "-ac", command_cpu, cmd], capture_output=True
+    )
     assert r.returncode == 0
 
     return r.stdout
@@ -42,7 +49,10 @@ def get_kernel_version():
 
 def get_thp_config():
     try:
-        output = subprocess.check_output(["cat", "/sys/kernel/mm/transparent_hugepage/enabled"], universal_newlines=True)
+        output = subprocess.check_output(
+            ["cat", "/sys/kernel/mm/transparent_hugepage/enabled"],
+            universal_newlines=True,
+        )
         if "[always]" in output.strip():
             return "THP_always"
         elif "[madvise]" in output.strip():
@@ -62,10 +72,25 @@ def get_result_filename():
     d = datetime.datetime.now()
     timestamp = d.strftime("%Y-%m-%d-%H-%M-%S")
 
-    return f'paper_results/{kernel}/{kernel}_{thp_config}_app_{timestamp}.csv'
+    return f"paper_results/{kernel}/{kernel}_{thp_config}_app_{timestamp}.csv"
+
+
+def get_init_df(times):
+    load_iter_times = ["load_time_iter_" + str(i) for i in range(times)]
+    run_iter_times = ["run_time_iter_" + str(i) for i in range(times)]
+
+    return pd.DataFrame(
+        columns=["name", "times"]
+        + load_iter_times
+        + ["avg_load_time"]
+        + run_iter_times
+        + ["avg_run_time"]
+    )
+
 
 def bench():
-    for name, info in benchmarks.items():
+    df = get_init_df(RUNTIMES)
+    for i, (name, info) in enumerate(benchmarks.items()):
         bench_path, times, regex_load_time, regex_run_time = info
         bench_real_path = os.path.join(SCRIPT_DIR, bench_path)
 
@@ -77,37 +102,46 @@ def bench():
 
         print(f"Running {name} for {times} times...")
 
-        total_load_time = 0.0
-        total_run_time = 0.0
+        # total_load_time = 0.0
+        # total_run_time = 0.0
+        load_times = []
+        run_times = []
 
         for t in range(times):
-            print(f"{t + 1}...", flush=True, end='')
-            output = run_command(bench_real_path).decode('utf-8')
+            print(f"{t + 1}...", flush=True, end="")
+            output = run_command(bench_real_path).decode("utf-8")
             if r_load:
                 load_time = float(r_load.search(output).group(1))
-                total_load_time += load_time
+                # total_load_time += load_time
+            else:
+                load_time = np.nan
+
             if r_run:
                 run_time = float(r_run.search(output).group(1))
-                total_run_time += run_time
+            else:
+                run_time = np.nan
 
-        if total_load_time > 0.0:
-            avg_load_time = total_load_time / times
-        else:
-            avg_load_time = "NA"
-        if total_run_time > 0.0:
-            avg_run_time = total_run_time / times
-        else:
-            avg_run_time = "NA"
+            load_times.append(load_time)
+            run_times.append(run_time)
+
+        avg_load_time = sum(load_times) / len(load_times) 
+        avg_run_time = sum(run_times) / len(run_times) 
 
         print("")
         print(f"Average load time: {avg_load_time}")
         print(f"Average run time: {avg_run_time}")
 
-        with open(FILENAME, 'a') as f:
-            f.write(f"{name},{times},{avg_load_time},{avg_run_time}\n")
+        df.loc[i] = [name, times] + load_times + [avg_load_time] + run_times + [avg_run_time]
+
+        # with open(FILENAME, "a") as f:
+        #     f.write(f"{name},{times},{avg_load_time},{avg_run_time}\n")
+
+    print(df)
+    df.to_csv(FILENAME, index=False)
 
 
 if __name__ == "__main__":
     FILENAME = get_result_filename()
     SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
     bench()
+    print(f"Results written to {FILENAME}")