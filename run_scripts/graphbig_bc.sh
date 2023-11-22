20dcaadcbf391215ff4d2c0f455cb8e886a29900
Siyuan Chai <siyuanc3@illinois.edu>
2023-11-22 02:24:42 -0500

    fix run script for perf with graphbig applications

--- run_scripts/graphbig_bc.sh
+++ run_scripts/graphbig_bc.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-
+set -x
 # dataset="graphBIG/dataset/small/"
 dataset_10k="graphBIG/dataset/LDBC/output-10k/"
-run_10k() {
-	"$1" --dataset $(realpath "$dataset_10k")
-}
 
-run_10k ./graphBIG/benchmark/bench_betweennessCentr/bc 
+perf_ctrl_fifo=$1
+ack_fifo=$2
+
+if [[ -n "$perf_ctrl_fifo" && -n "$ack_fifo" ]]; then
+    ./graphBIG/benchmark/bench_betweennessCentr/bc --dataset $(realpath "$dataset_10k") --perf_ctrl_fifo $perf_ctrl_fifo --perf_ack_fifo $ack_fifo
+else
+    ./graphBIG/benchmark/bench_betweennessCentr/bc --dataset $(realpath "$dataset_10k")
+fi
--- run_scripts/graphbig_dfs.sh
+++ run_scripts/graphbig_dfs.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-
+perf_ctrl_fifo=$1
+ack_fifo=$2
 # dataset="graphBIG/dataset/small/"
 dataset_1000k="graphBIG/dataset/LDBC/output-1000k/"
-run_1000k() {
-	"$1" --dataset $(realpath "$dataset_1000k")
-}
 
-run_1000k ./graphBIG/benchmark/bench_DFS/dfs
+if [[ -n "$perf_ctrl_fifo" && -n "$ack_fifo" ]]; then
+    ./graphBIG/benchmark/bench_DFS/dfs --dataset $(realpath "$dataset_1000k") --perf_ctrl_fifo $perf_ctrl_fifo --perf_ack_fifo $ack_fifo
+else
+    ./graphBIG/benchmark/bench_DFS/dfs --dataset $(realpath "$dataset_1000k")
+fi
