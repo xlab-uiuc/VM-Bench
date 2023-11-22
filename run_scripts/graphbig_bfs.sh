9da6d4c5089c96b746a3486c12b5c059588d558e
Siyuan Chai <siyuanc3@illinois.edu>
2023-11-22 02:24:42 -0500

    fix run script for perf with graphbig applications

--- run_scripts/graphbig_bfs.sh
+++ run_scripts/graphbig_bfs.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
-set -x
+
 perf_ctrl_fifo=$1
 ack_fifo=$2
-# dataset="graphBIG/dataset/small/"
-dataset_1000k="graphBIG/dataset/LDBC/output-100k/"
-# run_1000k() {
-# 	"$1" --dataset $(realpath "$dataset_1000k") --perf_ctrl_fifo $2
-# }
+dataset_1000k="graphBIG/dataset/LDBC/output-1000k/"
 
-# run_1000k ./graphBIG/benchmark/bench_BFS/bfs $perf_ctrl_fifo
-
-if [ -z "$perf_ctrl_fifo" ]; then
-    ./graphBIG/benchmark/bench_BFS/bfs --dataset $(realpath "$dataset_1000k")
-else
+if [[ -n "$perf_ctrl_fifo" || -n "$ack_fifo" ]]; then
     ./graphBIG/benchmark/bench_BFS/bfs --dataset $(realpath "$dataset_1000k") --perf_ctrl_fifo $perf_ctrl_fifo --perf_ack_fifo $ack_fifo
+else
+    ./graphBIG/benchmark/bench_BFS/bfs --dataset $(realpath "$dataset_1000k")
 fi
--- run_scripts/graphbig_pagerank.sh
+++ run_scripts/graphbig_pagerank.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-
-# dataset="graphBIG/dataset/small/"
+perf_ctrl_fifo=$1
+ack_fifo=$2
 dataset_1000k="graphBIG/dataset/LDBC/output-1000k/"
-run_1000k() {
-	"$1" --dataset $(realpath "$dataset_1000k")
-}
 
-run_1000k ./graphBIG/benchmark/bench_pageRank/pagerank
+if [[ -n "$perf_ctrl_fifo" || -n "$ack_fifo" ]]; then
+    ./graphBIG/benchmark/bench_pageRank/pagerank --dataset $(realpath "$dataset_1000k") --perf_ctrl_fifo $perf_ctrl_fifo --perf_ack_fifo $ack_fifo
+else
+    ./graphBIG/benchmark/bench_pageRank/pagerank --dataset $(realpath "$dataset_1000k")
+fi
