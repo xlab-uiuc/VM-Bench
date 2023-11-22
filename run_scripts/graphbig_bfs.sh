8f08757b498f343723ca5b18b4ca671691cb1949
Siyuan <siyuanc3@illinois.edu>
2023-11-21 23:15:53 -0600

    Enable perf ctl fifo and ack fifo. Tested with GraphBig BFS

--- run_scripts/graphbig_bfs.sh
+++ run_scripts/graphbig_bfs.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -x
 perf_ctrl_fifo=$1
+ack_fifo=$2
 # dataset="graphBIG/dataset/small/"
-dataset_1000k="graphBIG/dataset/LDBC/output-1000k/"
+dataset_1000k="graphBIG/dataset/LDBC/output-100k/"
 # run_1000k() {
 # 	"$1" --dataset $(realpath "$dataset_1000k") --perf_ctrl_fifo $2
 # }
@@ -12,5 +13,5 @@ dataset_1000k="graphBIG/dataset/LDBC/output-1000k/"
 if [ -z "$perf_ctrl_fifo" ]; then
     ./graphBIG/benchmark/bench_BFS/bfs --dataset $(realpath "$dataset_1000k")
 else
-    ./graphBIG/benchmark/bench_BFS/bfs --dataset $(realpath "$dataset_1000k") --perf_ctrl_fifo $perf_ctrl_fifo
+    ./graphBIG/benchmark/bench_BFS/bfs --dataset $(realpath "$dataset_1000k") --perf_ctrl_fifo $perf_ctrl_fifo --perf_ack_fifo $ack_fifo
 fi
