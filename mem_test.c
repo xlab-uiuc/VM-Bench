454f64598ac7527d2192361a9af2e60a77658e16
Siyuan Chai <siyuanc3@illinois.edu>
2023-11-30 16:21:27 -0600

    add mem_test and shutdown util

--- mem_test.c
+++ mem_test.c
@@ -2,13 +2,53 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <sys/mman.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+
+static void enable_perf(int perf_ctl_fd, int perf_ack_fd)
+{
+    char ack[5];
+    if (perf_ctl_fd != -1)
+    {
+        ssize_t bytes_written = write(perf_ctl_fd, "enable\n", 8);
+        assert(bytes_written == 8);
+    }
+    if (perf_ack_fd != -1)
+    {
+        ssize_t bytes_read = read(perf_ack_fd, ack, 5);
+        assert(bytes_read == 5 && strcmp(ack, "ack\n") == 0);
+    }
+    __asm__ volatile("xchgq %r10, %r10");
+}
+
+static void disable_perf(int perf_ctl_fd, int perf_ack_fd)
+{
+    char ack[5];
+    __asm__ volatile("xchgq %r11, %r11");
+    if (perf_ctl_fd != -1)
+    {
+        ssize_t bytes_written = write(perf_ctl_fd, "disable\n", 9);
+        assert(bytes_written == 9);
+    }
+
+    if (perf_ack_fd != -1)
+    {
+        ssize_t bytes_read = read(perf_ack_fd, ack, 5);
+        assert(bytes_read == 5 && strcmp(ack, "ack\n") == 0);
+    }
+}
 
 int main()
 {
     void * addr_start = (void *) 0x400000000001;
-    size_t len = (1ULL << 30); /* 1GB */
+    size_t len = (1ULL << 20); /* 1GB */
     uint32_t step = 0x1000;
-    
+
+    int perf_ctl_fd = -1;
+    int perf_ack_fd = -1;
+
     volatile uint8_t *a = mmap(addr_start, len, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
 
     if (a == MAP_FAILED) {
@@ -19,20 +59,17 @@ int main()
     printf("n mem refs =%lx\n", len / step);
     printf("a at %llx\n", (unsigned long long) a);
     for (int i = 0; i < len ; i += step) {
-        a[i + 5] = 5;
         a[i] = 0;
     }
+    
+    enable_perf(perf_ack_fd, perf_ack_fd);
 
     for (int i = 0; i < len ; i += step) {
-        a[i + 128] = 64;
+        uint32_t* p = (uint32_t*) &a[i];
+        *p = 64;
+        sum += a[i];
     }
-    
-    for (int j = 0; j < 1000; j++) {
-        for (int i = 0; i < len - step ; i += step) {
-            // sum += a[i] * a[i + step];
-            sum += a[i] + a[i + step + 5];
-        }
-    }
+    disable_perf(perf_ack_fd, perf_ack_fd);
 
     printf("sum=%lx\n", sum);
     return 0;
