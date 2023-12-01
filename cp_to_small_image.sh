1c60a254a9cc19966fb6d45a4708b3eff819c2de
Siyuan Chai <siyuanc3@illinois.edu>
2023-12-01 15:07:36 -0600

    fix small image mounting failure

--- cp_to_small_image.sh
+++ cp_to_small_image.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 set -x
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <filename> <loop_device>"
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <filename>"
     exit 1
 fi
 
 FILE_NAME="$1"  # Use the first argument as the filename
-LOOP_DEV="$2"   # Use the second argument as the loop device
 
 
 BASEDIR=$(pwd)
 MOUNT_PATH="/mnt/image"
 BIN_NAME=$(basename "$FILE_NAME" | cut -f 1 -d '.')
 
-sudo mount "$LOOP_DEV" "$MOUNT_PATH"
-# WAIT FOR FS SYNCING ? 
-sleep 5
+# use -o loop to mount a file as a block device
+# avoid setting up a loop device manually
+sudo mount -o loop ../image.ext4 "$MOUNT_PATH"
+
+# This is because the mounted image was corrupted.
+# Fixed with e2fsck
+# sleep 5
+# sync
 
 sudo cp "$FILE_NAME" "$MOUNT_PATH/"
 
