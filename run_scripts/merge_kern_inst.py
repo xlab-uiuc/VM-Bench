7aa1b50632d848f420edbc99c16566c3995300a6
Siyuan Chai <siyuanc3@illinois.edu>
2024-04-04 20:38:29 -0500

    High level kern inst breakdown script and data

---
+++ run_scripts/merge_kern_inst.py
@@ -0,0 +1,295 @@
+import glob
+import os
+import re
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import pandas as pd
+# matplotlib.use("TkAgg")
+
+def read_kernel_symbols(file_path='/proc/kallsyms'):
+    symbols2tiime = {}
+    
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                parts = line.strip().split()
+                if len(parts) >= 3:
+                    address, type, name = parts[:3]
+                    symbols2tiime[name] = 0
+                    # print(f"Address: {address}, Type: {type}, Name: {name}")
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except PermissionError:
+        print(f"Permission denied. You might need to run this as root to access {file_path}.")
+
+    return symbols2tiime
+
+def extract_number_at_end(line):
+    match = re.search(r'\d+$', line)
+    if match:
+        return int(match.group())
+    else:
+        return None
+
+def split_line_remove_number(line):
+    # Remove the trailing number using regular expression
+    line_without_number = re.sub(r'\s*\d+$', '', line)
+    # Split the line by semicolon
+    parts = line_without_number.split(';')
+    return parts
+
+
+def find_symbol(line, symbols):
+    blacklisted_symbols = ['entry_SYSCALL_64', 'do_syscall_64',
+                           'entry_SYSCALL_64_after_hwframe', 'strcmp', 'memmove', 'memset']
+    parts = split_line_remove_number(line)
+    for part in parts:
+        if part in symbols and (part not in blacklisted_symbols):
+            return part
+    return None
+
+def dict_to_df(symbols, column):
+    df = pd.DataFrame.from_dict(symbols, orient='index')
+    # df.reset_index(inplace=True)
+    # df.columns = ['symbol', 'time']
+    df.columns = [column]
+    df = df[df != 0].dropna()
+    # print(df)
+    return df
+
+def get_kernel_component_time(folded_file_path, kernel_symbols, column_key):
+
+    with open(folded_file_path, 'r') as file:
+        for line in file:
+            number = extract_number_at_end(line)
+            symbol = find_symbol(line, kernel_symbols)
+            if symbol:
+                kernel_symbols[symbol] += number
+
+    return dict_to_df(kernel_symbols, column_key)
+
+# color = [
+# 	# ['#1D3557', '#000000', '///'],
+# 	# ['#A8DADC', '#000000', '///'],
+# 	# ['#073B3A', '#000000', 'xxx'],
+# 	# ['#95B46A', '#000000', 'xxx'],
+# 	# ['#B87A00', '#000000', '...'],
+# 	# ['#FFC800', '#000000', '...'],
+# 	# ['#8C2F39', '#000000', '|||'],
+# 	# ['#FBA69D', '#000000', '|||'],
+
+# 	['#A8DADC', '#000000', '/'],
+# 	['#FFFFFF', '#000000', '/'],
+# 	['#95B46A', '#000000', 'x'],
+# 	['#FFFFFF', '#000000', 'x'],
+# 	['#FFC800', '#000000', '.'],
+# 	['#FFFFFF', '#000000', '.'],
+# 	['#FBA69D', '#000000', '|'],
+# 	['#FFFFFF', '#000000', '|'],
+# ]
+
+def merge_cross_kernel(symbol_to_time_list, kernels, key_col='mean'):
+    avg_list = [df[key_col].copy() for df in symbol_to_time_list]
+    all_kernel_perf = pd.concat(avg_list, axis=1)
+
+    all_kernel_perf.columns = kernels
+    all_kernel_perf.fillna(0,inplace=True)
+    return all_kernel_perf
+
+def process_cross_kernel_perf_breakdown(all_kernel_perf, kernels):
+    all_kernel_perf.sort_index(inplace=True)
+
+    print(all_kernel_perf)
+    label = all_kernel_perf.index
+    values = all_kernel_perf[kernels]
+
+    percentage = values / values.sum(axis=0)
+    small_idx = percentage < 0.004
+
+    # remove entries that are too small
+    percent_cleaned = percentage[~small_idx].dropna(how='all')
+    # append those small entries to the last entry
+    other_sums = percentage[small_idx].sum(axis=0).rename('others')
+    percent_cleaned = percent_cleaned._append(other_sums)
+
+    percent_cleaned.index = np.array([element.lstrip("__") for element in percent_cleaned.index])
+    
+    percent_cleaned.columns = ['EMT-Radix', 'Vanilla Linux']
+    return percent_cleaned
+
+def show_plot(all_kernel_perf, kernels, thp, test_name):
+    plt.figure(figsize=(5, 4))  # Set the size of the figure
+    plt.rcParams.update({'font.family': 'Times New Roman' })
+
+    clean_all_kernel_perf = process_cross_kernel_perf_breakdown(all_kernel_perf, kernels)
+    print(clean_all_kernel_perf)
+
+    categories = clean_all_kernel_perf.columns
+    
+    labels = clean_all_kernel_perf.index
+    prev = np.zeros(len(categories))
+    for i in range(len(labels)):
+        cur_data = np.array(clean_all_kernel_perf.iloc[i])        
+        plt.bar(categories, cur_data, bottom=prev, width=0.5, label=labels[i])
+        prev += cur_data
+
+    plt.title(f"Kernel Time Distribution for {test_name} on {thp}")  # Add a title to the chart
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=2)  # Add a legend
+    plt.tight_layout()
+    save_path = f'{thp}_{test_name}.svg'
+    print("save to ", save_path)
+    # plt.show(block=True)
+    plt.savefig(save_path)
+
+def get_files_with_prefix(directory, prefix):
+    pattern = os.path.join(directory, prefix + '*' + 'out_selected.folded')
+    # print(pattern)
+    # Use glob to find files that match the pattern
+    files = glob.glob(pattern)
+    files.sort()
+    return files
+
+def get_test_keyword(path, test_name):
+    start_idx = path.index(test_name)
+    path = path[start_idx:]
+    path = path.replace('_out_selected.folded', '')
+    return path
+
+def merge_results(result_path_list, test_name):
+    # result_list is a list of (bench_names, runtimes) tuples
+    # merge the results into a single dataframe
+    # return the merged dataframe
+    merged = pd.DataFrame()
+
+    sybmol2time = read_kernel_symbols()
+    for path in result_path_list:
+        keyword = get_test_keyword(path, test_name)
+        kernel_time_df = get_kernel_component_time(path, sybmol2time.copy(), keyword)
+        
+        merged = pd.concat([merged, kernel_time_df], axis=1)
+        
+    # append the mean column
+    merged.fillna(0,inplace=True)
+    merged['mean'] = merged.mean(axis=1) 
+    return merged
+
+def merge_per_thp_per_test(per_thp_test_dfs, key):
+    merged = pd.DataFrame()
+    for df in per_thp_test_dfs:
+        merged = pd.concat([merged, df], axis=1)
+    merged.fillna(0,inplace=True)
+    
+    # print(merged)
+
+    # get relative base
+    base_sum = merged[key].sum()
+    relative = merged / base_sum
+
+    # print(relative)
+    small_idx = relative < 0.004
+
+    percent_cleaned = relative[~small_idx].dropna(how='all')
+    # append those small entries to the last entry
+    other_sums = relative[small_idx].sum(axis=0).rename('others')
+    percent_cleaned = percent_cleaned._append(other_sums)
+    percent_cleaned.index = np.array([element.lstrip("__") for element in percent_cleaned.index])
+
+    print(percent_cleaned)
+    
+    return percent_cleaned
+
+def get_result_per_kernel_thp_test(kernel, thp, test_name):
+    folder = f'kernel_inst'
+    file_prefix = f'{kernel}_{thp}_{test_name}'
+
+    # example radix_never_graphbig_tc_walk_log.bin.kern_inst.folded.high_level.csv
+    # paper_results/5.15.0-vanilla/LEBench/
+    result_path = os.path.join(folder, file_prefix + '_walk_log.bin.kern_inst.folded.high_level.csv')
+
+    if 'graphbig' in test_name:
+        test_name = test_name.replace('graphbig_', '')
+    df = pd.read_csv(result_path, header=None, names=['symbol', f'{kernel}_{test_name}'])
+    df.set_index('symbol', inplace=True)
+    return df
+
+    # print('\n'.join(result_path_list))
+
+    # print(merged)
+
+def show_relative_plot(per_thp_reults, thp):
+    plt.figure(figsize=(10, 4))  # Set the size of the figure
+    # plt.rcParams.update({'font.family': 'Times New Roman' })
+
+    merged = pd.DataFrame()
+    for df in per_thp_reults:
+        merged = pd.concat([merged, df], axis=1)
+    merged.fillna(0,inplace=True)
+
+    print(merged)
+
+    categories = merged.columns
+    
+    labels = merged.index
+    prev = np.zeros(len(categories))
+    for i in range(len(labels)):
+        cur_data = np.array(merged.iloc[i])        
+        plt.bar(categories, cur_data, bottom=prev, width=0.5, label=labels[i])
+        prev += cur_data
+
+    plt.title(f"Kernel Time Distribution for on {thp}")  # Add a title to the chart
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=2)  # Add a legend
+    plt.tight_layout()
+    save_path = f'{thp}_kern_inst.svg'
+    print("save to ", save_path)
+    # # plt.show(block=True)
+    plt.savefig(save_path)
+
+
+if __name__ == '__main__':
+    plt.ion() 
+
+    archs = [
+        "radix",
+        "ecpt",
+    ]
+
+    THP_options = [
+        # "always",
+        "never",
+    ]
+
+    tests = [
+        "graphbig_bfs" ,
+        "graphbig_cc" ,
+        "graphbig_dc" ,
+        "graphbig_dfs" ,
+        # "graphbig_sssp" ,
+        "graphbig_tc" ,
+        "graphbig_pagerank" ,
+        # "sysbench" ,
+        # "gups",
+    ]
+
+    
+    for thp in THP_options:
+        per_thp = []
+        for test in tests:  
+            per_thp_test = []
+            for kernel in archs:
+                df = get_result_per_kernel_thp_test(kernel, thp, test)
+                per_thp_test.append(df)
+
+            base_key = f'radix_{test}'
+            if 'graphbig' in test:
+                stripped = test.replace('graphbig_', '')
+                key = f'radix_{stripped}'
+
+            test_df = merge_per_thp_per_test(per_thp_test, key)
+            per_thp.append(test_df)
+
+        show_relative_plot(per_thp, thp)
+                # perf_df.append(merged)
+
+
+            # 
