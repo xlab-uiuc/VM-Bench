5881515737d35e77d2a1397a79e9f0ca3e531dcb
Siyuan Chai <siyuanc3@illinois.edu>
2024-10-14 23:43:56 -0500

    postgres standalone script

---
+++ run_scripts/postgres_st.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+
+[ ! $1 ] && OUTPUT_FOLDER="output" || OUTPUT_FOLDER=$1
+
+kernel=`uname -r`
+content=`cat /sys/kernel/mm/transparent_hugepage/enabled`
+
+if [[ $content == *"[always]"* ]]; then
+    THP_CONFIG="THP_always"
+elif [[ $content == *"[madvise]"* ]]; then
+    THP_CONFIG="THP_madvise"
+elif [[ $content == *"[never]"* ]]; then
+    THP_CONFIG="THP_never"
+else
+    echo "Unable to determine the status of Transparent Huge Pages."
+fi
+
+timestamp=`date +%Y-%m-%d_%H-%M-%S`
+OUTPUT_FOLDER=paper_results/$OUTPUT_FOLDER-${kernel}-${THP_CONFIG}-${timestamp}
+echo "Output folder: $OUTPUT_FOLDER"
+mkdir -p $OUTPUT_FOLDER
+chmod +666 $OUTPUT_FOLDER
+
+cd postgresql-14.13/build_dir/bin
+
+ITERATION=2
+for ((i=0;i<$ITERATION;i++)); do
+    sudo numactl --cpunodebind=0 --membind=0 --physcpubind=8 su postgres -c './postgres --single -D /data1/siyuan_pgsql/data/ postgres -R 12000000' \
+        2>&1 | tee $OUTPUT_FOLDER/postgres_${kernel}_${THP_CONFIG}_standalone_iter${i}.txt
+
+    sleep 3
+done
+
