8dca34387e3d4c6f9a62133be18fada81e402a5b
Siyuan Chai <siyuanc3@illinois.edu>
2024-11-06 13:25:44 -0600

    clean up kernel_inst; old data likely from loading stage. moved to thp_always_archived

--- kernel_inst/cp_helper.sh
+++ kernel_inst/cp_helper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-file_list=$(ls | grep sysbench)
+file_list=$(ls | grep always | grep -v postgres | grep -v redis)
 
 for file in ${file_list[@]}; do
     if [ -f $file ]; then
         echo "Processing $file"
     fi
-    cp $file ../kernel_inst_high_level/
+    mv $file thp_always_archived/
 done
